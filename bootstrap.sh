888852ccf59c0f2b67ea0702c4c95e5855dedca2
Markus Dulghier <markus@dulghier.com>
2014-08-04 19:07:26 +0200

    fix symlink

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 DOTFILES_ROOT=$(pwd)
 
 # Vim
-ln -s $HOME/.vimrc $DOTFILES_ROOT/.vimrc
+ln -s $DOTFILES_ROOT/.vimrc $HOME/.vimrc 
 mkdir -p ~/.vim/bundle ~/.vim/swaps ~/.vim/undo ~/.vim/backups
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
